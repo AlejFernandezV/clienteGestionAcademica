2bcce9b5b4cbe393552c06b7e16c7621a83e5d33
William Diaz <wrdiaz@unicauca.edu.co>
2023-10-24 23:11:33 -0500

    first changes

--- src/app/layouts/admin-layout/admin-layout.module.ts
+++ src/app/layouts/admin-layout/admin-layout.module.ts
@@ -6,11 +6,11 @@ import { AdminLayoutRoutes } from './admin-layout.routing';
 import { DashboardComponent } from '../../dashboard/dashboard.component';
 import { UserProfileComponent } from '../../user-profile/user-profile.component';
 import { TableListComponent } from '../../table-list/table-list.component';
-import { TypographyComponent } from '../../typography/typography.component';
-import { IconsComponent } from '../../icons/icons.component';
-import { MapsComponent } from '../../maps/maps.component';
-import { NotificationsComponent } from '../../notifications/notifications.component';
-import { UpgradeComponent } from '../../upgrade/upgrade.component';
+//import { TypographyComponent } from '../../typography/typography.component';
+//import { IconsComponent } from '../../icons/icons.component';
+//import { MapsComponent } from '../../maps/maps.component';
+//import { NotificationsComponent } from '../../notifications/notifications.component';
+//import { UpgradeComponent } from '../../upgrade/upgrade.component';
 import {MatButtonModule} from '@angular/material/button';
 import {MatInputModule} from '@angular/material/input';
 import {MatRippleModule} from '@angular/material/core';
@@ -35,11 +35,11 @@ import {MatSelectModule} from '@angular/material/select';
     DashboardComponent,
     UserProfileComponent,
     TableListComponent,
-    TypographyComponent,
+    /*TypographyComponent,
     IconsComponent,
     MapsComponent,
     NotificationsComponent,
-    UpgradeComponent,
+    UpgradeComponent,*/
   ]
 })
 
