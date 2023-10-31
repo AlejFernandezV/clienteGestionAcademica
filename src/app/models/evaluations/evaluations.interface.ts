1332918b92275ee38baf81eeb1de8126c8b0dace
William Diaz <wrdiaz@unicauca.edu.co>
2023-10-30 20:38:44 -0500

    add backend on evaluation table

---
+++ src/app/models/evaluations/evaluations.interface.ts
@@ -0,0 +1,10 @@
+export interface EvaluationI {   
+    eva_estado: string;
+    eva_puntaje: number;
+    eva_resultado: string;
+    lab_horas: number;
+    lab_nombre: string;
+    per_fecha_fin: string;
+    per_fecha_inicio: string;
+    tl_descripcion: string;
+}
--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EvaluationsService } from 'app/services/evaluations.service';
+import { EvaluationI } from 'app/models/evaluations/evaluations.interface';
+
 
 @Component({
   selector: 'app-table-list',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./table-list.component.css']
 })
 export class TableListComponent implements OnInit {
+  evaluaciones: EvaluationI [] = [];
+  totalHoras: number = 0 ;
 
-  constructor() { }
+  constructor(private evalutionsService: EvaluationsService) { }
 
   ngOnInit() {
+    this.getEvaluation()
   }
 
+  getEvaluation() {
+    this.evalutionsService.getEvaluation().subscribe((data: any) => {
+      this.evaluaciones = data.results;
+
+      for (let i = 0; i < this.evaluaciones.length; i++) { 
+        this.totalHoras += this.evaluaciones[i].lab_horas; 
+      }
+      console.log(this.totalHoras)
+      console.log(this.evaluaciones)
+
+    });
+  }
 }
