26ba3affdda2c051c6908f4486506f8be30fe5fd
William Diaz <wrdiaz@unicauca.edu.co>
2023-11-05 16:42:21 -0500

    change on UI

--- src/app/table-list/table-list.component.ts
+++ src/app/table-list/table-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EvaluationsService } from 'app/services/evaluations.service';
 import { EvaluationI } from 'app/models/evaluations/evaluations.interface';
+import { data } from 'jquery';
 
 
 @Component({
@@ -16,18 +17,22 @@ export class TableListComponent implements OnInit {
 
   ngOnInit() {
     this.getEvaluation()
+    this.getTotalHours()
   }
 
   getEvaluation() {
     this.evalutionsService.getEvaluation().subscribe((data: any) => {
       this.evaluaciones = data.results;
+      console.log(this.evaluaciones)
+    });
+  }
 
+  getTotalHours(){
+    this.evalutionsService.getEvaluation().subscribe(() =>{
       for (let i = 0; i < this.evaluaciones.length; i++) { 
         this.totalHoras += this.evaluaciones[i].lab_horas; 
       }
       console.log(this.totalHoras)
-      console.log(this.evaluaciones)
-
     });
   }
 }
