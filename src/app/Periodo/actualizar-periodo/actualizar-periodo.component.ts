b2ed9aad09c93c36dc41b8dbf37edefc5aec5d04
Dufray93 <108368940+Dufray93@users.noreply.github.com>
2023-11-17 09:34:12 -0500

    nuevos cambios

--- src/app/Periodo/actualizar-periodo/actualizar-periodo.component.ts
+++ src/app/Periodo/actualizar-periodo/actualizar-periodo.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { PeriodoI } from '../../models/periodo/periodo.interface';
+import { PeriodosService } from '../../services/periodos.service';
 
 @Component({
   selector: 'app-actualizar-periodo',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ActualizarPeriodoComponent implements OnInit {
 
-  constructor() { }
+  periodo: PeriodoI = {
+    per_nombre: '',
+    per_fechainicio: null,
+    per_fechafin: null
+  }
+
+  constructor(private PeriodoService: PeriodosService,
+    private router: Router,
+    private activeRoute: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const nomP = <string>this.activeRoute.snapshot.params.id;
+
+    if (nomP) {
+      this.PeriodoService.getPeriodo(nomP).subscribe(
+        res => {
+          this.periodo = res[0];
+          console.log(res[0]);
+        },
+        err => console.log(err)
+      );
+    }
+  }
+  updatePeriodo() {
+    this.PeriodoService.updatePeriodo(this.periodo.per_nombre, this.periodo).subscribe(
+      res => {
+        console.log(res);
+      },
+      err => console.log(err)
+    );
+    this.router.navigate(['/listar-periodo']);
   }
 
 }
