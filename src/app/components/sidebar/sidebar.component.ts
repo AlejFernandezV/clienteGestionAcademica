da9e5b9f4c9a3cb8e62553521c8755b6af059dcb
William Diaz <wrdiaz@unicauca.edu.co>
2023-11-21 22:43:43 -0500

    logout

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -9,7 +9,6 @@ declare interface RouteInfo {
 }
 export const ROUTES: RouteInfo[] = [
   
-<<<<<<< HEAD
     { path: '/dashboard', title: 'Dashboard',  icon: 'dashboard', class: '' },
     { path: '/user-profile', title: 'Perfil',  icon:'person', class: '' },
     { path: '/table-list', title: 'Evaluación',  icon:'content_paste', class: '' },
@@ -17,15 +16,6 @@ export const ROUTES: RouteInfo[] = [
     { path: '/listar-periodo', title: 'Periodo', icon: 'person', class: ''},
     { path: '/autoevaluacion', title: 'Autoevaluacion', icon: 'content_paste', class: ''},
     { path: '/listar-labor', title: 'Labor docente', icon: 'content_paste', class: ''}
-=======
-    { path: '/dashboard',               title: 'Dashboard',     icon: 'dashboard',    class: '' },
-    { path: '/user-profile',            title: 'Perfil',        icon:'person',        class: '' },
-    { path: '/table-list',              title: 'Evaluación',    icon:'content_paste', class: '' },
-    { path: '/Periodo/listar-periodo',  title: 'Periodo',       icon:'settings',      class: '' },
-    { path: '/docente',                 title: 'Docente',       icon:'group',         class: '' },
-    { path: '/l_docente/listar-labor',  title: 'Labor Docente', icon:'list_alt',      class: '' },
-    //{ path: '/autoevaluacion',          title: 'Autoevaluación',icon:'quiz',          class: '' },
->>>>>>> c35af17f76fb6c14301575a4862243844c9cb5fe
     
     //{ path: '/notifications', title: 'Notifications',  icon:'notifications', class: '' },
 ];
