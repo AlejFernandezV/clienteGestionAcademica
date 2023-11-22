fa579ae638779c8d52265cc09b8b726483ef0eda
William Diaz <wrdiaz@unicauca.edu.co>
2023-11-21 21:44:28 -0500

    logout

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -16,11 +16,8 @@ export class LoginService {
     //return this.http.post<ResponseI>('https://tw0l7qqd-3333.use2.devtunnels.ms/login', form); // portatil
     return this.http.post<ResponseI>('http://127.0.0.1:3333/login', form)
   }
-<<<<<<< HEAD
 
   onLogout(token: any): Observable<ResponseI>{
     return this.http.post<ResponseI>('https://nvxv3pn0-3333.use2.devtunnels.ms/logout', token.token);
   }
-=======
->>>>>>> c35af17f76fb6c14301575a4862243844c9cb5fe
 }
