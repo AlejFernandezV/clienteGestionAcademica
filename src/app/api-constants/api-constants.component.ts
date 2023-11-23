37049867fb82a121038a5b8c9978af058ceb3bb7
Dufray93 <108368940+Dufray93@users.noreply.github.com>
2023-11-23 08:15:45 -0500

    ajustes en periodo y usuarios

--- src/app/api-constants/api-constants.component.ts
+++ src/app/api-constants/api-constants.component.ts
@@ -1,1 +1,1 @@
-export const API_BASE_URL = 'https://apigestionacademica.onrender.com/';
+export const API_BASE_URL = 'https://apigestionacademica-b4jp.onrender.com/';
--- src/app/services/usuarios.service.ts
+++ src/app/services/usuarios.service.ts
@@ -3,29 +3,30 @@ import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { ResponseI } from 'app/models/response.interface';
 import { Observable, throwError, Subject } from "rxjs";
 import { UsuarioI } from '../models/usuario/usuario.interface';
+import { API_BASE_URL } from 'app/api-constants/api-constants.component';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
-  private apiUrl = '/api';
+  private apiUrl = `${API_BASE_URL}`;
 
   constructor(private http: HttpClient) { }
 
-  getUsuario(id: number) {
-    return this.http.get(`${this.apiUrl}/usuarios/${id}`);
+  getUsuario(id: number): Observable<ResponseI> {
+    return this.http.get<ResponseI>(`${this.apiUrl}/usuarios/${id}`);
   }
-  getUsuarios(){
-    return this.http.get(`${this.apiUrl}/usuarios`);
+  getUsuarios(): Observable<ResponseI> {
+    return this.http.get<ResponseI>(`${this.apiUrl}/usuarios`);
   }
-  deleteUsuario(id: number){
-    return this.http.delete(`${this.apiUrl}/usuarios/${id}`);
+  deleteUsuario(id: number): Observable<ResponseI> {
+    return this.http.delete<ResponseI>(`${this.apiUrl}/usuarios/${id}`);
   }
 
-  createUsuario(usuario: UsuarioI){
-    return this.http.post(`${this.apiUrl}/usuarios`, usuario);
+  createUsuario(usuario: UsuarioI): Observable<ResponseI> {
+    return this.http.post<ResponseI>(`${this.apiUrl}/usuarios`, usuario);
   }
-  updateUsuario(id: number, updateUsuario: UsuarioI){
-    return this.http.put(`${this.apiUrl}/usuarios/${id}`, updateUsuario);
+  updateUsuario(id: number, updateUsuario: UsuarioI): Observable<ResponseI> {
+    return this.http.put<ResponseI>(`${this.apiUrl}/usuarios/${id}`, updateUsuario);
   }
 }
